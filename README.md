253b2371fa308f695dbe2be4ffa2e93f952082f7
Daniele D'Orazio <d.dorazio96@gmail.com>
2018-11-26 15:40:44 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+<img src="https://cdn.rawgit.com/oh-my-fish/oh-my-fish/e4f1c2e0219a17e2c748b824004c8d0b38055c16/docs/logo.svg" align="left" width="144px" height="144px"/>
+
+#### sashimi
+> A theme for [Oh My Fish][omf-link].
+
+[![MIT License](https://img.shields.io/badge/license-MIT-007EC7.svg?style=flat-square)](/LICENSE)
+[![Fish Shell Version](https://img.shields.io/badge/fish-v2.2.0-007EC7.svg?style=flat-square)](https://fishshell.com)
+[![Oh My Fish Framework](https://img.shields.io/badge/Oh%20My%20Fish-Framework-007EC7.svg?style=flat-square)](https://www.github.com/oh-my-fish/oh-my-fish)
+
+<br/>
+
+
+## Install
+
+```fish
+$ omf install sashimi
+```
+
+
+## Features
+
+* Lorem ipsum dolor sit amet.
+* Consectetur adipisicing elit.
+
+
+## Screenshot
+
+<!-- <p align="center">
+<img src="{{TODO}}">
+</p> -->
+
+
+# License
+
+[MIT][mit] © [d-dorazio][author] et [al][contributors]
+
+
+[mit]:            https://opensource.org/licenses/MIT
+[author]:         https://github.com/d-dorazio
+[contributors]:   https://github.com/d-dorazio/theme-sashimi/graphs/contributors
+[omf-link]:       https://www.github.com/oh-my-fish/oh-my-fish
+
+[license-badge]:  https://img.shields.io/badge/license-MIT-007EC7.svg?style=flat-square
---
+++ fish_prompt.fish
@@ -0,0 +1,99 @@
+# Git
+function git::is_repo
+    test -d .git
+    or command git rev-parse --git-dir >/dev/null ^/dev/null
+end
+
+function git::branch_name
+    git::is_repo
+    and begin
+        command git symbolic-ref --short HEAD ^/dev/null
+
+        or command git show-ref --head -s --abbrev | head -n1 ^/dev/null
+    end
+end
+
+function git::is_dirty
+    git::is_repo
+    and not command git diff --no-ext-diff --quiet --exit-code
+end
+
+function git::is_staged
+    git::is_repo
+    and begin
+        not command git diff --cached --no-ext-diff --quiet --exit-code
+    end
+end
+
+function git::is_stashed
+    git::is_repo
+    and begin
+        command git rev-parse --verify --quiet refs/stash >/dev/null
+    end
+end
+
+function git::is_touched
+    git::is_repo
+    and begin
+        test -n (echo (command git status --porcelain))
+    end
+end
+
+function git::untracked
+    git::is_repo
+    and begin
+        command git ls-files --other --exclude-standard
+    end
+end
+
+function fish_prompt
+    set -l symbol "λ "
+    set -l code $status
+
+    printf (dim)(prompt_pwd)" "(off)
+
+    if git::is_repo
+        set -l branch (git::branch_name ^/dev/null)
+        set -l ref (git show-ref --head --abbrev | awk '{print substr($0,0,7)}' | sed -n 1p)
+
+        if command git symbolic-ref HEAD >/dev/null ^/dev/null
+            if git::is_staged
+                printf (cyan)"$branch"(off)
+            else
+                printf (yellow)"$branch"(off)
+            end
+        else
+            printf (dim)"$ref"(off)
+        end
+
+        if git::is_dirty
+            printf (red)"*"(off)
+        end
+
+        for remote in (git remote)
+            set -l behind_count (echo (command git rev-list $branch..$remote/$branch ^/dev/null | wc -l | tr -d " "))
+            set -l ahead_count (echo (command git rev-list $remote/$branch..$branch ^/dev/null | wc -l | tr -d " "))
+
+            if test $ahead_count -ne 0
+                or test $behind_count -ne 0
+                and test (git remote | wc -l) -gt 1
+                echo -n -s " "(orange)$remote(off)
+            end
+
+            if test $ahead_count -ne 0
+                echo -n -s (white)" +"$ahead_count(off)
+            end
+
+            if test $behind_count -ne 0
+                echo -n -s (white)" -"$behind_count(off)
+            end
+        end
+    end
+
+    if test "$code" = 0
+        echo -n -s (dim)" $symbol"(off)
+    else
+        echo -n -s (red)" $symbol"(off)
+    end
+
+end
